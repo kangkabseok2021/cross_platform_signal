44666e6bb22b29208b66bd86ab9d38459ae90fea
Kab Seok Kang <83808276+kangkabseok2021@users.noreply.github.com>
2026-06-26 23:13:18 +0200

    chore(cv-telemetry): scaffold directory + conanfile.py + CMakePresets

---
+++ cv_websocket_telemetry/conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
+
+
+class CvWsTelemetryConan(ConanFile):
+    name = "cv_ws_telemetry"
+    version = "1.0.0"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def requirements(self):
+        self.requires("opencv/4.9.0")
+        self.requires("boost/1.85.0")
+        self.requires("nlohmann_json/3.11.3")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def layout(self):
+        cmake_layout(self)
